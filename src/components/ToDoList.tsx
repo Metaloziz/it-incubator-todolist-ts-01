e720d0790b7ef7654547c99a6498c14ffc72d49f
Metaloziz <tesla9760614>
2021-12-18 22:30:21 +0300

    ToDo_create_firs_component

---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const ToDoList = () => {
+    return (<div className="App">
+            <div>
+                <h3>What to learn</h3>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
+        </div>
+
+    )
+}
