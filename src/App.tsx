85563673ad99040f9ea8d39f0e256fc552a31e0e
Metaloziz <tesla9760614>
2021-12-19 19:22:47 +0300

    ToDo_5_don't_work_create_one_ass_arr

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ import s from './App.module.css'
 
 export type filterPT = 'All' | 'Completed' | 'Active'
 
-export type tasksPT = taskPT[]
+export type tasksPT = {
+    [key: string]: taskPT[]
+}
 
 export type taskPT = {
     id: string
@@ -36,27 +38,30 @@ const buttons = {
 
 function App() {
 
-    const toDoLists: statePT = [
-        {id: v1(), title: 'What to learn', filter: 'All'},
-        {id: v1(), title: 'Food', filter: 'All'}
-    ]
+    const list1 = v1()
+    const list2 = v1()
 
-    const tasks1: tasksPT = [
-        {id: v1(), title: 'JS', isDone: false},
-        {id: v1(), title: 'TS', isDone: true},
-        {id: v1(), title: 'NodeJS', isDone: false},
-        {id: v1(), title: 'AI', isDone: true},
-        {id: v1(), title: 'Skynet', isDone: false}
-    ]
-    const tasks2: tasksPT = [
-        {id: v1(), title: 'Apple', isDone: false},
-        {id: v1(), title: 'Pear', isDone: false},
-        {id: v1(), title: 'Melon', isDone: false},
-        {id: v1(), title: 'Orange', isDone: false}
-    ]
+    const [state, setState] = useState<statePT>([
+        {id: list1, title: 'What to learn', filter: 'All'},
+        {id: list2, title: 'Food', filter: 'All'}
+    ])
 
-    const [tasks, setTasks] = useState<tasksPT>(tasks1)
-    const [state, setState] = useState<statePT>(toDoLists)
+
+    const [tasks, setTasks] = useState<tasksPT>({
+        [list1]: [
+            {id: v1(), title: 'JS', isDone: false},
+            {id: v1(), title: 'TS', isDone: true},
+            {id: v1(), title: 'NodeJS', isDone: false},
+            {id: v1(), title: 'AI', isDone: true},
+            {id: v1(), title: 'Skynet', isDone: false}
+        ],
+        [list2]: [
+            {id: v1(), title: 'Apple', isDone: false},
+            {id: v1(), title: 'Pear', isDone: false},
+            {id: v1(), title: 'Melon', isDone: false},
+            {id: v1(), title: 'Orange', isDone: false}
+        ]
+    })
 
 
     const removeTask = (taskID: string) => {
@@ -69,31 +74,31 @@ function App() {
         setTasks(copyTasks)
     }
     const changeTaskStatus = (taskID: string) => {
-        let copyTasks: tasksPT = [...tasks]
+        let copyTasks = [...tasks]
         copyTasks = tasks.map(l => l.id === taskID ? {...l, isDone: !l.isDone} : l)
         setTasks(copyTasks)
     }
-    const changeFilter = (filter: filterPT, idToDoList: string) => {
-        console.log('gg')
-        debugger
-        let copyToDoLists = [...toDoLists]
-        copyToDoLists.map(l => l.id === idToDoList ? {...l, filter: filter} : l)
-    }
 
+    const changeFilter = (filter: filterPT, idToDoList: string) => {
+        let changeToDo = state.find(l => l.id === idToDoList)
+        if (changeToDo) {
+            changeToDo.filter = filter
+        }
+        setState([...state])
+    }
 
     return (
         <div className={s.main}>
             {state.map(l => {
 
                 let copyTasks = tasks
-                debugger
+
                 switch (l.filter) {
                     case 'Completed':
-                        copyTasks = tasks.filter(l => l.isDone)
+                        copyTasks = copyTasks.filter(l => l.isDone)
                         break
                     case 'Active':
-                        copyTasks = tasks.filter(l => !l.isDone)
-                        break
+                        copyTasks = copyTasks.filter(l => !l.isDone)
                 }
 
                 return (<ToDoList toDoListID={l.id}
